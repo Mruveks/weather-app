a376f3ad34fa92e3e2f22099fedc07edeae45a02
Kuba Mrowiec <mruvekss@gmail.com>
2023-02-23 12:21:26 +0100

    Updated Media Page

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -1,10 +1,78 @@
 import React from 'react'
+import photo1 from '../assets/photo-1.jpg'
+import photo2 from '../assets/photo-2.jpg'
+import photo3 from '../assets/photo-3.jpg'
+import photo4 from '../assets/photo-4.jpg'
+import photo5 from '../assets/photo-5.jpg'
+import photo6 from '../assets/photo-6.jpg'
+import photo7 from '../assets/photo-7.jpg'
+import photo8 from '../assets/photo-8.jpg'
+import photo9 from '../assets/photo-9.jpg'
+import photo10 from '../assets/photo-10.jpg'
+
+import photo11 from '../assets/photo-11.jpg'
+import photo12 from '../assets/photo-12.jpg'
+import photo13 from '../assets/photo-13.jpg'
+import photo14 from '../assets/photo-14.jpg'
+import photo15 from '../assets/photo-15.jpg'
+import photo16 from '../assets/photo-16.jpg'
+import photo17 from '../assets/photo-17.jpg'
+import photo18 from '../assets/photo-18.jpg'
+import photo19 from '../assets/photo-19.jpg'
+import photo20 from '../assets/photo-20.jpg'
+
+import photo21 from '../assets/photo-21.jpg'
+import photo22 from '../assets/photo-22.jpg'
+import photo23 from '../assets/photo-23.jpg'
+import photo24 from '../assets/photo-24.jpg'
+import photo25 from '../assets/photo-25.jpg'
+import photo26 from '../assets/photo-26.jpg'
+import photo27 from '../assets/photo-27.jpg'
+import photo28 from '../assets/photo-28.jpg'
+import photo29 from '../assets/photo-29.jpg'
+
 
 const media = () => {
   return (
-    <div>
+    <div className="flex sm:mx-40 gap-10 sm:mt-10">
+      <div className="flex flex-col flex-wrap gap-10">
+        <img src={photo11} alt="cloud" />   
+        <img src={photo12} alt="cloud" />    
+        <img src={photo13} alt="cloud" />       
+        <img src={photo14} alt="cloud" />       
+        <img src={photo15} alt="cloud" />       
+        <img src={photo16} alt="cloud" />       
+        <img src={photo17} alt="cloud" />        
+        <img src={photo18} alt="cloud" />        
+        <img src={photo19} alt="cloud" />     
+      </div>
+      
+      <div className="flex flex-col flex-wrap gap-10">
+        <img src={photo21} alt="cloud" />   
+        <img src={photo22} alt="cloud" />    
+        <img src={photo23} alt="cloud" />       
+        <img src={photo24} alt="cloud" />       
+        <img src={photo25} alt="cloud" />       
+        <img src={photo26} alt="cloud" />       
+        <img src={photo27} alt="cloud" />        
+        <img src={photo8} alt="cloud" />            
+      </div>
+
+      <div className="flex flex-col flex-wrap gap-10">
+        <img src={photo1} alt="cloud" />   
+        <img src={photo10} alt="cloud"/>    
+        <img src={photo3} alt="cloud" />       
+        <img src={photo4} alt="cloud" />       
+        <img src={photo5} alt="cloud" />       
+        <img src={photo6} alt="cloud" />       
+        <img src={photo7} alt="cloud" />        
+        <img src={photo28} alt="cloud" />        
+        <img src={photo9} alt="cloud" />
+        <img src={photo20} alt="cloud" /> 
+      </div>
       
     </div>
+
   )
 }
 
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -6,12 +6,13 @@ const News = () => {
   const [data, setData] = useState([])
 
   useEffect(() => {
-    axios.get('https://api.newscatcherapi.com/v2/latest_headlinesz', {
+    axios.get('https://api.newscatcherapi.com/v2/latest_headlines', {
       params: {
         countries: 'pl', topic: 'news', page: '1'
       },
       headers: {
-        'x-api-key': 'GCTLiIPqEnQwll6ycKmWnigy94frBROcuy_RFrzCBd0'
+        "Access-Control-Allow-Origin": '*',
+        'x-api-key': `GCTLiIPqEnQwll6ycKmWnigy94frBROcuy_RFrzCBd0`
       }
     })
       .then(res => {
@@ -41,7 +42,7 @@ const News = () => {
                 <p>{item.author}</p>
               </div>
               <a href={item.link} className="text-4xl font-bold hover:text-blue-500 hover:cursor-pointer py-2">{item.title}</a>
-              <p className="py-2">{item.excerpt.length > 10 ? item.excerpt : null}</p>
+              <p className="py-2">{item.excerpt > 10 ? item.excerpt : null}</p>
               <p>{item.published_date}</p>
             </div>
           </div>
