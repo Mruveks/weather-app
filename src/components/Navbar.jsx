d8c54bf7e43c204806a96600d476439086b732f1
Kuba Mrowiec <mruvekss@gmail.com>
2023-06-10 09:23:23 +0200

    Update Logo and animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import {
 	AiOutlineFacebook,
 	AiOutlineYoutube,
 } from "react-icons/ai";
+import WeatherGraph from "../assets/WeatherGraph.png";
 
 const Navbar = () => {
 	const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
@@ -13,8 +14,7 @@ const Navbar = () => {
 
 	const handleScroll = () => {
 		const currentScrollPos = window.pageYOffset;
-		const isVisible =
-			prevScrollPos > currentScrollPos;
+		const isVisible = prevScrollPos > currentScrollPos;
 
 		setPrevScrollPos(currentScrollPos);
 		setVisible(isVisible);
@@ -32,7 +32,9 @@ const Navbar = () => {
 			} `} w-screen fixed top-0`}
 		>
 			<div className="h-14 bg-gray-50 flex justify-between items-center sm:px-10 px-40 text-4xl text-black">
-				<NavLink to="/">WeatherGraph</NavLink>
+				<NavLink to="/">
+					<img src={WeatherGraph} alt="logo"/>
+				</NavLink>{" "}
 				<div className="flex sm:hidden gap-8 text-2xl">
 					<AiOutlineInstagram className="cursor-pointer" />
 					<AiOutlineFacebook className="cursor-pointer" />
@@ -40,11 +42,10 @@ const Navbar = () => {
 					<AiOutlineYoutube className="cursor-pointer" />
 				</div>
 			</div>
-			<div className="h-14 flex justify-between bg-orange-400 sm:px-10 px-40 pr-[50%] items-center text-3xl">
-        <NavLink to="/forecast">Forecast</NavLink>
-        <NavLink to="/historical">Historical Weather Data</NavLink>
-        <NavLink to="/forecast">Map</NavLink>
-
+			<div className="h-14 flex justify-between bg-orange-400 sm:px-10 px-40 pr-[50%] items-center text-4xl">
+				<NavLink to="/forecast" className="hover-underline-animation">Forecast</NavLink>
+				<NavLink to="/historical" className="hover-underline-animation">Historical Weather Data</NavLink>
+				<NavLink to="/forecast" className="hover-underline-animation">Map</NavLink>
 			</div>
 		</nav>
 	);
